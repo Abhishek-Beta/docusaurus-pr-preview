161dd50129d4e84d88fdca31d51e55895958e16c
Abhishek <abhishek@betalectic.com>
2024-10-10 17:13:20 +0530

    Changes made

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: "https://your-docusaurus-site.example.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: process.env.GITHUB_ACTIONS ? `${process.env.BASE_URL}/` : "/",
+  baseUrl: "/docusorus-pr-preview/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
